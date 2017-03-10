3a9185bd150b541ee43e3b758a05ca48ff6e98d9
Amanda Koster <amandakoster@gmail.com>
2017-03-09 17:32:46 -0800

    Updated local

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var name = prompt('what is your name?');
 alert('Welcome ' + name + '!');
 console.log('Welcome ' + name + '!');
 
+function answer2() {
 var answer2 = prompt('Do you think Amanda is a native Seattleite?');
 console.log(answer2);
 if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
@@ -15,7 +16,10 @@ if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
 } else {
   alert(name + ' ,' + 'Ya gotta play to get to the end. Humor me!');
 }
+}
+answer2();
 
+function turnSignal() {
 var answer3 = prompt('Not sure? Ok. Do I use my turn signal while driving?');
 console.log(answer3);
 if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
@@ -26,7 +30,10 @@ if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
 } else {
   alert('You didn\'t put in an answer!');
 }
+}
+turnSignal()
 
+function umbrella() {
 var answer4 = prompt('Ok. Do I use an umbrella?');
 console.log(answer4);
 if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
@@ -37,7 +44,10 @@ if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
 } else {
   alert('C\'mon, just answer the question!');
 }
+}
+umbrella();
 
+function sub() {
 var answer5 = prompt('Last question. Do I say sub or hogie??');
 console.log(answer5);
 if (answer5.toLowerCase() === 'sub' || answer5.toLowerCase() === 'S'){
@@ -48,7 +58,10 @@ if (answer5.toLowerCase() === 'sub' || answer5.toLowerCase() === 'S'){
 } else {
   alert('You didn\'t put in a proper answer!');
 }
+}
+sub();
 
+function twentySeven() {
 for (var i = 0; i < 5; i++){
   var answer = prompt('Pick a number between 1 and 50');
   console.log(typeof answer);
@@ -62,7 +75,10 @@ for (var i = 0; i < 5; i++){
     console.log(answer);
   }
 }
+}
+twentySeven();
 
+function color() {
 for (var i = 0; i < 6; i++){
   var answer = prompt('What is one color of the Romanian flag?');
   var possibleAnswers = ['red','yellow','blue'];
@@ -78,5 +94,8 @@ for (var i = 0; i < 6; i++){
     console.log(answer);
   }
 }
+}
+color();
+
 alert('The colors of the Romanian flag are red, yellow, blue!');
 alert('Awesome, ' + name + '! You got ' + correct + ' out of 7 questions correct!');
